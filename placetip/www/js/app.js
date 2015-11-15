210f9593f4e7a9ff68f32e30374ac8ce9b461dba
mathon_j <mathon_j@epitech.eu>
2015-11-15 08:44:22 +0100

    up geolocation + icons

--- placetip/www/js/app.js
+++ placetip/www/js/app.js
@@ -1,12 +1,15 @@
 function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
+//    navigator.splashscreen.hide();
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
-function onDeviceReady() {
-    navigator.splashscreen.hide();
-    navigator.geolocation.getCurrentPosition(function(position){
-        initMap(position.coords.latitude, position.coords.longitude);
-    });
+var onSuccess = function(position){
+  initMap(position.coords.latitude, position.coords.longitude);
+};
+
+function onError(error) {
+    console.log('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
 }
 
 var app = angular.module('app', []);
